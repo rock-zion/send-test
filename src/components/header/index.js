605f827ea1a0691ceac9519139dd17576931ca9c
rock-zion <rock_zion@outlook.com>
2022-03-24 16:37:41 +0100

    add responsiveness to header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import style from './style.module.css';
+import hamburger from '../../assets/hamburger.svg';
+import { useSidebar } from 'contexts/sidebar';
+
 export const Header = ({ title, children }) => {
+  const { sidebarOpen, toggleSidebar } = useSidebar();
   return (
     <header className={style['header']}>
       <div className='flex item-center'>
+        <button
+          className={style['hamburger']}
+          onClick={() => toggleSidebar(!sidebarOpen)}>
+          <img src={hamburger} alt='sidebar toggle menu' />
+        </button>
+
         <h1 className={style['title']}>{title}</h1>
       </div>
 
