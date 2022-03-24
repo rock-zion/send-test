6ee1517c1dc8ee219c3febe00191d1245e54adc9
rock-zion <rock_zion@outlook.com>
2022-03-24 16:29:12 +0100

    add skeleton ui fallback

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Suspense, lazy } from 'react';
+import { MainFallback } from 'components/loading/main-fallback';
 const Main = lazy(() => {
   return new Promise(resolve => {
     setTimeout(() => resolve(import('./pages/main')), 2000);
   });
 });
+      <Suspense fallback={<MainFallback />}>
