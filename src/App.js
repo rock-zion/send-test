fc0d293c8c035c8c33f2dc9968a754883a2f13b3
rock-zion <rock_zion@outlook.com>
2022-03-24 16:36:11 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import './styles/index.css';
 import React, { Suspense, lazy } from 'react';
+import { Navigate, Routes, Route } from 'react-router-dom';
 import { MainFallback } from 'components/loading/main-fallback';
+
 const Main = lazy(() => {
   return new Promise(resolve => {
     setTimeout(() => resolve(import('./pages/main')), 2000);
   });
 });
+// const Main = lazy(() => import('./pages/main'));
+
+function App() {
+  return (
+    <div className='App'>
       <Suspense fallback={<MainFallback />}>
+        <Routes>
+          <Route path='/' element={<Navigate to='/main/quotes' replace />} />
+          <Route path='/main/*' element={<Main />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
